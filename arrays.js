95bd528ba64e6e8e2c4d1dee5ce5ea647af7d0c6
Ismail Jalloh <ijalloh21@gmail.com>
2018-08-17 02:39:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
+  array = array.slice(0)
   return array
 }
 
