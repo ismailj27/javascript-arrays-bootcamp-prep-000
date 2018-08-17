c7cd3bfd2c52d8198da394a7a99396db9d094157
Ismail Jalloh <ijalloh21@gmail.com>
2018-08-17 02:36:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element,...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+} 
+
+function addElementToEndOfArray(array, element) {
+  array2 = [...array,element]
+  return array2
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array[array.length] = element
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(array.length * (-1))
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.shift(array.length)
+  return array
+}
+
+function RemoveElementFromEndOfArray(array) {
+  array.slice(array.length -1)
+  return array
+}
+
